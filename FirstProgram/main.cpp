261d25f5ba047cebfa613ca4cc91ca99d8d5c572
VivekSM1992 <smvivek92hegde@gmail.com>
2024-12-01 18:35:58 +0530

    First programe and excercise

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+    int favorite_number = 0;
+    std::cout <<"Enter your favorite nummber between 1 and 100: ";
+    std::cin>>favorite_number;
+    std::cout <<"Amazing!! Thats my favorite number too!"<<std::endl;
+    std::cout<<favorite_number;
+    std::cout<<" is my favorite number!"<< std::endl;
+
+    return 0;
+}
